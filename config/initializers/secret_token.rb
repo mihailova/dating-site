f18ac0754e96549576050e86b787a125ca478b5f
Inna Mihailova <innna.p.m@gmail.com>
2013-10-17 17:59:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DatingSite::Application.config.secret_key_base = '0598806132c9e5ec564f607731d5486fc71f3b63f2bdde29938014893eae62c941120be7c0e1f26e18edf986a0c0d54b77b097a3d087a260f564086844b9bb7c'

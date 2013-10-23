6c20c2be201fda18e6f5be6dfc5cd90467c4969c
Inna Mihailova <innna.p.m@gmail.com>
2013-10-23 15:04:55 +0100

    Add name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  acts_as_messageable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def mailboxer_email(object)
+   	self.email
+   end
 end
